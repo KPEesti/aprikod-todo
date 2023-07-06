9afa274f43dacc9448551c32af426fa0a28c0b72
So_Green <iwantoseeyou1@gmail.com>
2023-07-06 21:40:12 +0500

    Add new logic to TodoDetail compnent

--- src/components/TodoDetail/index.tsx
+++ src/components/TodoDetail/index.tsx
@@ -1,16 +1,46 @@
 import {observer} from "mobx-react";
 import styles from "./todoDetail.module.scss";
+import React, {useContext, useState} from "react";
+import {StoreContext} from "../../App";
+import {createPortal} from "react-dom";
+import {TaskModal} from "../TaskModal";
 
 export const TodoDetail = observer(() => {
+    const store = useContext(StoreContext);
+
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+    const handleDeleteTask = () => {
+        console.log('Handle Delete');
+        store.deleteTask(store.currentTask!.id);
+    }
+
+
+    if (!store.currentTask) {
+        return (
+            <div>
+                <h2 className={styles.todoDetail__title}>Chose your task!</h2>
+            </div>
+        );
+    }
+
     return (
         <div className={styles.todoDetail}>
-            <h2 className={styles.todoDetail__title}>Title</h2>
-            <hr className={styles.todoDetail__divider}/>
-            <p className={styles.todoDetail__description}>Description</p>
-            <div className={styles.todoDetail__detailBlock}>
-                <span>Статус: Не закончена</span>
-                <span>Дата Создания: 21.12.2022</span>
+            <div className={styles.todoDetail__header}>
+                <h2 className={styles.todoDetail__title}>{store.currentTask?.title}</h2>
+                <div>
+                    <img className={styles.todoDetail__icons} src="/images/edit.svg" height="25px" alt=""/>
+                    <img className={styles.todoDetail__icons} onClick={handleDeleteTask} src="/images/trash.svg" height="25px" alt=""/>
+                </div>
             </div>
+            <hr className={styles.todoDetail__divider}/>
+            <p className={styles.todoDetail__description}>{store.currentTask?.description}</p>
+            <div className={styles.todoDetail__detailBlock}>
+                <span>Статус: {store.currentTask.completed ? "Завершена" : "Не завершена"}</span>
+                <span>Дата Создания: {store.currentTask?.creationDate.toUTCString()}</span>
+            </div>
+            <button onClick={() => setIsModalOpen(true)}>Добавить подзадачу</button>
+            {isModalOpen && createPortal(<TaskModal ctx={store.currentTask} closeModal={() => setIsModalOpen(false)}/>, document.body)}
         </div>
     );
 });
